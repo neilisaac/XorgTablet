f8f7ef6ec85af2e8475cdf638d94b2e48e9c6f24
Neil Isaac <isaac.neil@gmail.com>
2013-01-24 00:23:48 -0500

    Fixed regression: clicking works again

--- src/com/gimpusers/xorgtablet/XButtonEvent.java
+++ src/com/gimpusers/xorgtablet/XButtonEvent.java
@@ -2,6 +2,6 @@ package com.gimpusers.xorgtablet;
 
 public class XButtonEvent extends XEvent {
 	public XButtonEvent(int x, int y, int pressure, Button button, boolean down, boolean absolute) {
-		super(absolute ? Type.MOTION_ABSOLUTE : Type.MOTION_RELATIVE, x, y, pressure, button, down);
+		super(absolute ? Type.BUTTON_ABSOLUTE : Type.BUTTON_RELATIVE, x, y, pressure, button, down);
 	}
 }
